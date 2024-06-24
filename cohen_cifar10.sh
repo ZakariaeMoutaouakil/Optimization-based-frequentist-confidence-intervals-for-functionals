fb9611d39fd94c466e891560427a4e61e7940832
Zakariae Moutaouakil <moutaouakilzakariae.cpgea@gmail.com>
2024-06-24 11:35:38 +0200

    Changed comparison.sh

--- cohen_cifar10.sh
+++ cohen_cifar10.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Activate the virtual environment
+source venv/bin/activate
+
 # Set locale to ensure decimal point handling
 export LC_NUMERIC="C"
 export PYTHONPATH="$HOME/Optimization-based-frequentist-confidence-intervals-for-functionals"
